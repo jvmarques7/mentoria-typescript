72affbb10cf2021797fb6be49b8795afd71180b6
jvmarques7 <65418225+jvmarques7@users.noreply.github.com>
2023-04-12 18:37:56 -0300

    Update desafio3.js

--- src/desafios/desafio3.js
+++ src/desafios/desafio3.js
@@ -21,3 +21,10 @@ botaoAtualizar.addEventListener('click', function () {
 botaoLimpar.addEventListener('click', function () {
     limparSaldo();
 });
+
+/**
+    <h4>Valor a ser adicionado: <input id="soma"> </h4>
+    <button id="atualizar-saldo">Atualizar saldo</button>
+    <button id="limpar-saldo">Limpar seu saldo</button>
+    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
+ */
