06da1f389632eb7bb719c2a921e9b32860da5923
jvmarques7 <65418225+jvmarques7@users.noreply.github.com>
2023-04-12 18:35:45 -0300

    Update desafio3.js

--- src/desafios/desafio3.js
+++ src/desafios/desafio3.js
@@ -1,18 +1,17 @@
-// O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
-
 let botaoAtualizar = document.getElementById('atualizar-saldo');
 let botaoLimpar = document.getElementById('limpar-saldo');
 let soma = document.getElementById('soma');
 let campoSaldo = document.getElementById('campo-saldo');
 
-campoSaldo.innerHTML = 0
+campoSaldo.innerHTML = "0"
 
-function somarAoSaldo(soma) {
-    campoSaldo.innerHTML += soma;
+function somarAoSaldo(soma: string) {
+  let valorAtual = parseInt(campoSaldo.innerHTML) + parseInt(soma);
+  campoSaldo.innerHTML = valorAtual.toString()
 }
 
 function limparSaldo() {
-    campoSaldo.innerHTML = '';
+    campoSaldo.innerHTML = "";
 }
 
 botaoAtualizar.addEventListener('click', function () {
@@ -22,10 +21,3 @@ botaoAtualizar.addEventListener('click', function () {
 botaoLimpar.addEventListener('click', function () {
     limparSaldo();
 });
-
-/**
-    <h4>Valor a ser adicionado: <input id="soma"> </h4>
-    <button id="atualizar-saldo">Atualizar saldo</button>
-    <button id="limpar-saldo">Limpar seu saldo</button>
-    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
- */
